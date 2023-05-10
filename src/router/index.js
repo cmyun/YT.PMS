a4676853f5d23ca26eeeb5175f971fb7a814a1dd
pthphung <pthphung01@gmail.com>
2023-05-10 12:17:58 +0700

    add

--- src/router/index.js
+++ src/router/index.js
@@ -47,14 +47,14 @@ const router = createRouter({
   linkActiveClass: "active"
 });
 
-// router.beforeEach((to, from, next) => {
-//   const publicPages = ['/login', '/register'];
-//   const authRequired = !publicPages.includes(to.path);
-//   const loggedIn = localStorage.getItem('user');
-//   if (authRequired && !loggedIn) {
-//     return next('/login');
-//   }
-//   next();
-// })
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+  next();
+})
 
 export default router;
